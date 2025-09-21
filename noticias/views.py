e5371230f860e93ded543720e67d27ebea297197
Luan <luanpa082@gmail.com>
2025-09-21 12:15:10 -0300

    Subindo projeto completo para GitHub

--- noticias/views.py
+++ noticias/views.py
@@ -1,10 +1,32 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
-from django.db.models import Count
+from django.db.models import Count, F
+from django.core.cache import cache
 from .models import Noticia, Categoria
 
 
-class NoticiaListView(ListView):
+# 🔹 Mixin para centralizar categorias e populares
+class NoticiasBaseMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        categorias = cache.get('categorias')
+        if not categorias:
+            categorias = Categoria.objects.annotate(num_noticias=Count('noticia'))
+            cache.set('categorias', categorias, 300)  # 5 min de cache
+
+        populares = cache.get('noticias_populares')
+        if not populares:
+            populares = Noticia.objects.order_by('-visualizacoes')[:5]
+            cache.set('noticias_populares', populares, 300)
+
+        context['categorias'] = categorias
+        context['noticias_populares'] = populares
+        return context
+
+
+# 🔹 Lista de notícias (com filtro e ordenação)
+class NoticiaListView(NoticiasBaseMixin, ListView):
     model = Noticia
     template_name = 'noticias/index.html'
     context_object_name = 'noticias'
@@ -24,41 +46,35 @@ class NoticiaListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         categoria_id = self.request.GET.get('categoria')
-
-        context['categorias'] = Categoria.objects.annotate(num_noticias=Count('noticia'))
-        context['noticias_populares'] = Noticia.objects.order_by('-visualizacoes')[:5]
         context['categoria_selecionada'] = int(categoria_id) if categoria_id else 0
-
         return context
 
 
-class NoticiaDetailView(DetailView):
+# 🔹 Detalhe da notícia (com slug para SEO)
+class NoticiaDetailView(NoticiasBaseMixin, DetailView):
     model = Noticia
     template_name = 'noticias/detalhe.html'
     context_object_name = 'noticia'
-    pk_url_kwarg = 'id'
+    slug_url_kwarg = 'slug'
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        self.object.visualizacoes += 1
-        self.object.save(update_fields=['visualizacoes'])
+        # Incremento seguro usando F()
+        Noticia.objects.filter(pk=self.object.pk).update(visualizacoes=F('visualizacoes') + 1)
+        self.object.refresh_from_db(fields=['visualizacoes'])
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         noticia = self.object
         context['noticias_relacionadas'] = Noticia.objects.filter(
             categoria=noticia.categoria
         ).exclude(id=noticia.id).order_by('-data_publicacao')[:3]
-
-        context['categorias'] = Categoria.objects.annotate(num_noticias=Count('noticia'))
-        context['noticias_populares'] = Noticia.objects.order_by('-visualizacoes')[:5]
-
         return context
 
 
-class NoticiaPorCategoriaListView(ListView):
+# 🔹 Lista de notícias por categoria
+class NoticiaPorCategoriaListView(NoticiasBaseMixin, ListView):
     model = Noticia
     template_name = 'noticias/index.html'
     context_object_name = 'noticias'
@@ -71,7 +87,5 @@ class NoticiaPorCategoriaListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categoria'] = self.categoria
-        context['categorias'] = Categoria.objects.annotate(num_noticias=Count('noticia'))
-        context['noticias_populares'] = Noticia.objects.order_by('-visualizacoes')[:5]
         context['categoria_selecionada'] = self.categoria.id
         return context
--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-800m9*oh3qe6gaie!l4+2tf34w+=ha1a!_xtif!hr9m2(9xtom'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,15 +119,20 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+STATIC_URL = '/static/'
+
+# Pasta onde os arquivos coletados serão armazenados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Opcional: pastas extras de estáticos (geralmente já existe)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
