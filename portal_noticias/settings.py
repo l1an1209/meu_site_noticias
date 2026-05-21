87fb64d2a4694219201c8af493d534296114bd8a
Luan <luanpa082@gmail.com>
2026-05-21 20:54:21 -0300

    Configuração PostgreSQL Render (DATABASE_URL)

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -75,15 +75,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portal_noticias.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_noticias',
-        'USER': 'postgres',
-        'PASSWORD': 'luan',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
