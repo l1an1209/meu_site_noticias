509e4ebb0487c568faa35ab1677f698f2b24c035
Luan <luanpa082@gmail.com>
2026-05-22 01:40:53 -0300

    Migrations concluídas e sistema funcionando local

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -75,17 +75,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portal_noticias.wsgi.application'
 
+
+
+
 import dj_database_url
-import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
