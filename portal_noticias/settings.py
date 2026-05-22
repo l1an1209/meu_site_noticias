aa837309931dd0a34439acda9287c17e34e4eb59
Luan <luanpa082@gmail.com>
2026-05-22 02:13:17 -0300

    update

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -81,14 +81,16 @@ WSGI_APPLICATION = 'portal_noticias.wsgi.application'
 
 import dj_database_url
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 AUTH_PASSWORD_VALIDATORS = [
