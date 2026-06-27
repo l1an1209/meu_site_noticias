b9a315c2e7a0834e0751016f67afa35e9fefd6e6
Luan <luanpa082@gmail.com>
2026-06-27 20:14:50 -0300

    Configura PostgreSQL no Render

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -81,18 +81,27 @@ WSGI_APPLICATION = 'portal_noticias.wsgi.application'
 
 
 
-
+import os
 import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=False,
-    )
-}
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
