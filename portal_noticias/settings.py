e9dfabdbbb4f0f9fa3e6be96a8b4208509b08f17
Luan <luanpa082@gmail.com>
2026-06-27 20:00:36 -0300

    Atualiza portal de notícias com melhorias e novas funcionalidades

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -87,17 +87,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
-
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get(
-            'DATABASE_URL',
-            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-        ),
+    "default": dj_database_url.config(
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=False,
     )
 }
 AUTH_PASSWORD_VALIDATORS = [
